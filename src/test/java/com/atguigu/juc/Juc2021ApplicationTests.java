dddff5dcbee859293037f5f1d55abd27fd44ab80
shuhongfan <shfshuhongfan@gmail.com>
2023-04-09 12:03:22 +0800

    init

---
+++ src/test/java/com/atguigu/juc/Juc2021ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.atguigu.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Juc2021ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
